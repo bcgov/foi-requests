b3bab72d28aac0d16227e6aa60132ad3df3db3c5
Mark Voorberg <mark@binaryops.ca>
2019-02-19 23:19:32 -0800

    Added ministries and alert styles

--- src/app/route-components/ministry-confirmation/ministry-confirmation.component.ts
+++ src/app/route-components/ministry-confirmation/ministry-confirmation.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
+import { Observable, of } from 'rxjs';
+import { FoiRequest } from 'src/app/models/FoiRequest';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './ministry-confirmation.component.html',
   styleUrls: ['./ministry-confirmation.component.scss']
 })
 export class MinistryConfirmationComponent implements OnInit {
-
   @ViewChild(BaseComponent) base: BaseComponent;
+  foiForm = this.fb.group({
+    // values include [general, personal]
+    requestType: [null, [Validators.required]]
+  });
 
-  constructor() {}
+  foiRequest: FoiRequest;
+  foiForm$: Observable<any>;
+  ministries$: Observable<any>;
+
+  constructor(private fb: FormBuilder, private dataService: DataService) {}
 
   ngOnInit() {
+    this.foiRequest = this.dataService.getCurrentState();
+    console.log('Loaded ministryConfirmation:', this.foiRequest.requestData);
+
+    this.ministries$ = this.dataService.getMinistries();
+    // The template does the actual subscribe, when that happens
+    // this line will ensure that the reactive form is populated with data.
+    this.foiForm$ = of(
+      this.foiRequest.requestData.requestType || {}
+    ).pipe(tap(data => this.foiForm.patchValue(data)));
   }
 
   doContinue() {
@@ -20,5 +41,4 @@ export class MinistryConfirmationComponent implements OnInit {
   doGoBack() {
     this.base.goFoiBack();
   }
-
 }
