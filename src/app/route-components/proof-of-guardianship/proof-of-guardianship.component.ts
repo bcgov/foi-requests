9cc1396901f4f4ad9822a6eb25a8bcafdb309e12
Mark Voorberg <mark@binaryops.ca>
2019-02-26 14:37:24 -0800

    Proof of guardianship

--- src/app/route-components/proof-of-guardianship/proof-of-guardianship.component.ts
+++ src/app/route-components/proof-of-guardianship/proof-of-guardianship.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { BaseComponent } from '../base/base.component';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { BaseComponent } from "../base/base.component";
+import { FoiRequest } from "src/app/models/FoiRequest";
+import { FormBuilder, Validators } from "@angular/forms";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
-  templateUrl: './proof-of-guardianship.component.html',
-  styleUrls: ['./proof-of-guardianship.component.scss']
+  templateUrl: "./proof-of-guardianship.component.html",
+  styleUrls: ["./proof-of-guardianship.component.scss"]
 })
 export class ProofOfGuardianshipComponent implements OnInit {
-
   @ViewChild(BaseComponent) base: BaseComponent;
+  foiForm = this.fb.group({
+    hasGuardianship: [null, [Validators.required]]
+  });
 
-  constructor() {}
+  foiRequest: FoiRequest;
+  targetKey: string = "proofOfGuardianship";
+
+  constructor(private fb: FormBuilder, private dataService: DataService) {}
 
   ngOnInit() {
+    // Load the current values & populate the FormGroup.
+    this.foiRequest = this.dataService.getCurrentState(this.targetKey);
+    this.foiForm.patchValue(this.foiRequest.requestData[this.targetKey]);
   }
 
   doContinue() {
-    this.base.goFoiForward();
+    // Update save data & proceed.
+    const state = this.dataService.setCurrentState(this.foiRequest, this.targetKey, this.foiForm);
+    this.base.goFoiForward(state.requestData[this.targetKey].requestType);
   }
+
   doGoBack() {
     this.base.goFoiBack();
   }
-
 }
