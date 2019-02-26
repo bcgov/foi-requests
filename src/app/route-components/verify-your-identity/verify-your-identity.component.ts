f4430328382cd0550ea4da670c5597f576a683b0
Wiebo Troost <wiebo@binaryops.ca>
2019-02-26 14:03:52 -0800

    verify your identity

--- src/app/route-components/verify-your-identity/verify-your-identity.component.ts
+++ src/app/route-components/verify-your-identity/verify-your-identity.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
+import { Validators, FormBuilder } from '@angular/forms';
+import { FoiRequest } from 'src/app/models/FoiRequest';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   templateUrl: './verify-your-identity.component.html',
@@ -9,14 +12,39 @@ export class VerifyYourIdentityComponent implements OnInit {
 
   @ViewChild(BaseComponent) base: BaseComponent;
 
-  constructor() {}
+  foiRequest: FoiRequest;
+
+  foiForm = this.fb.group({
+    firstName: [null, Validators.compose([Validators.required, Validators.maxLength(255)])],
+    middleName:  [null, [Validators.maxLength(255)]],
+    lastName:  [null, Validators.compose([Validators.required, Validators.maxLength(255)])],
+    birthDate:  [null, [Validators.required]],
+    alsoKnownAs:  [null, Validators.compose([Validators.maxLength(255)])],
+    businessName:  [null, [Validators.maxLength(255)]]
+  });
+
+  constructor(private fb: FormBuilder, private dataService: DataService) {}
 
   ngOnInit() {
+    this.foiRequest = this.dataService.getCurrentState();
+    this.foiRequest.requestData.personalInfo =
+      this.foiRequest.requestData.personalInfo || {};
+    this.foiForm.patchValue(this.foiRequest.requestData.personalInfo);
   }
 
   doContinue() {
+    // Copy out submitted form data.
+    this.foiRequest.requestData.personalInfo = {};
+    const formData = this.foiForm.value;
+    Object.keys(formData).map(
+      k => (this.foiRequest.requestData.personalInfo[k] = formData[k])
+    );
+
+    // Update save data & proceed.
+    this.dataService.setCurrentState(this.foiRequest);
     this.base.goFoiForward();
   }
+
   doGoBack() {
     this.base.goFoiBack();
   }
