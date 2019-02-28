411b2616311284107c473c22ed0be9725b7173c8
Wiebo Troost <wiebo@binaryops.ca>
2019-02-28 13:02:00 -0800

    infoBlock on verify your identity

--- src/app/route-components/verify-your-identity/verify-your-identity.component.ts
+++ src/app/route-components/verify-your-identity/verify-your-identity.component.ts
@@ -1,45 +1,48 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { BaseComponent } from '../base/base.component';
-import { Validators, FormBuilder } from '@angular/forms';
-import { FoiRequest } from 'src/app/models/FoiRequest';
-import { DataService } from 'src/app/services/data.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { BaseComponent } from "../base/base.component";
+import { Validators, FormBuilder } from "@angular/forms";
+import { FoiRequest } from "src/app/models/FoiRequest";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
-  templateUrl: './verify-your-identity.component.html',
-  styleUrls: ['./verify-your-identity.component.scss']
+  templateUrl: "./verify-your-identity.component.html",
+  styleUrls: ["./verify-your-identity.component.scss"]
 })
 export class VerifyYourIdentityComponent implements OnInit {
-
   @ViewChild(BaseComponent) base: BaseComponent;
 
   foiRequest: FoiRequest;
-  targetKey: string = 'contactInfo';
+  targetKey: string = "contactInfo";
+  infoBlock: string;
 
   foiForm = this.fb.group({
     firstName: [null, Validators.compose([Validators.required, Validators.maxLength(255)])],
-    middleName:  [null, [Validators.maxLength(255)]],
-    lastName:  [null, Validators.compose([Validators.required, Validators.maxLength(255)])],
-    birthDate:  [null, [Validators.required]],
-    alsoKnownAs:  [null, Validators.compose([Validators.maxLength(255)])],
-    businessName:  [null, [Validators.maxLength(255)]]
+    middleName: [null, [Validators.maxLength(255)]],
+    lastName: [null, Validators.compose([Validators.required, Validators.maxLength(255)])],
+    birthDate: [null, [Validators.required]],
+    alsoKnownAs: [null, Validators.compose([Validators.maxLength(255)])],
+    businessName: [null, [Validators.maxLength(255)]]
   });
 
   constructor(private fb: FormBuilder, private dataService: DataService) {}
 
   ngOnInit() {
     this.foiRequest = this.dataService.getCurrentState();
-    this.foiRequest.requestData[this.targetKey] =
-      this.foiRequest.requestData[this.targetKey] || {};
+    this.foiRequest.requestData[this.targetKey] = this.foiRequest.requestData[this.targetKey] || {};
     this.foiForm.patchValue(this.foiRequest.requestData[this.targetKey]);
+
+    this.base.getFoiRouteData().subscribe(data => {
+      if (data) {
+        this.infoBlock = data.infoBlock;
+      }
+    });
   }
 
   doContinue() {
     // Copy out submitted form data.
     this.foiRequest.requestData[this.targetKey] = {};
     const formData = this.foiForm.value;
-    Object.keys(formData).map(
-      k => (this.foiRequest.requestData[this.targetKey][k] = formData[k])
-    );
+    Object.keys(formData).map(k => (this.foiRequest.requestData[this.targetKey][k] = formData[k]));
 
     // Update save data & proceed.
     this.dataService.setCurrentState(this.foiRequest);
@@ -50,4 +53,3 @@ export class VerifyYourIdentityComponent implements OnInit {
     this.base.goFoiBack();
   }
 }
-
