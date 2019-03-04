f6d56f6219935d7bed9abca7b5a9c99be5af9171
Mark Voorberg <mark@binaryops.ca>
2019-03-04 10:13:15 -0800

    update Karma conf

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,12 +20,18 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: [ '--remote-debugging-port=9333' ]
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeDebugging'],
     singleRun: false
   });
 };
