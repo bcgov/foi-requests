52c76f4cad72632abbb099b5da3e3ba5287f9674
Mark Voorberg <mark@binaryops.ca>
2019-03-07 10:40:46 -0800

    Checking that the title is set

--- src/app/core-header/core-header.component.spec.ts
+++ src/app/core-header/core-header.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CoreHeaderComponent } from './core-header.component';
+import { CoreHeaderComponent } from "./core-header.component";
 
-describe('CoreHeaderComponent', () => {
+describe("CoreHeaderComponent", () => {
   let component: CoreHeaderComponent;
   let fixture: ComponentFixture<CoreHeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoreHeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [CoreHeaderComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,13 @@ describe('CoreHeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should have the correct title", () => {
+    const coreHeader = fixture.debugElement.componentInstance;
+    const domElement = fixture.debugElement.nativeElement;
+    expect(domElement.querySelector("span").innerText).toBe(coreHeader.serviceName);
   });
 });
