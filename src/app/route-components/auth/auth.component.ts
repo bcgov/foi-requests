80d27d9737eaa3804304838ae9cb86413ee981bc
saravankumarpa <saravanpa@gmail.com>
2020-02-17 11:59:38 -0800

    adding login WIP

--- src/app/route-components/auth/auth.component.ts
+++ src/app/route-components/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { BaseComponent } from 'src/app/utils-components/base/base.component';
 import { FoiRequest } from 'src/app/models/FoiRequest';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
+import {startWith} from "rxjs/operators";
 
 @Component({
   templateUrl: './auth.component.html',
@@ -11,32 +12,47 @@ import { DataService } from 'src/app/services/data.service';
 export class AuthComponent implements OnInit {
   @ViewChild(BaseComponent) base: BaseComponent;
   foiForm = this.fb.group({
-    auth: [null, [Validators.required]]
+    answerYes: [null, [Validators.required]]
   });
-
   foiRequest: FoiRequest;
-  targetKey: string = 'auth';
-
+  answerYes: boolean = null;
+  answerReceived: boolean;
+  continuetext = 'Start your request'
+  targetKey: string;
   constructor(private fb: FormBuilder, private dataService: DataService) {}
 
+
   ngOnInit() {
-    // Load the current values & populate the FormGroup.
-    this.foiRequest = this.dataService.getCurrentState(this.targetKey);
-    this.foiForm.patchValue(this.foiRequest.requestData[this.targetKey]);
-  }
+    this.base.getFoiRouteData().subscribe(data => {
+      if (data) {
+        // Load the current values & populate the FormGroup.
+        this.foiRequest = this.dataService.getCurrentState(this.targetKey);
+        const initialValues = this.foiRequest.requestData[this.targetKey];
+        this.foiForm.patchValue(initialValues);
 
+        this.foiForm.valueChanges.pipe(startWith(initialValues)).subscribe(newValue => {
+          this.answerReceived = this.answerYes !== null;
+          this.answerYes = newValue.answerYes === 'true';
+          if (this.answerYes) {
+            this.continuetext = 'Login With BC Service Card';
+          } else {
+            this.continuetext = 'Continue without Logging In';
+          }
+          this.base.continueDisabled = !this.answerYes;
+        });
+      }
+    });
+  }
   doContinue() {
-    // Update save data & proceed.
-    const state = this.dataService.setCurrentState(
-      this.foiRequest,
-      this.targetKey,
-      this.foiForm
-    );
-    this.base.goFoiForward(state.requestData[this.targetKey].auth);
+    console.log('-------------try')
+    this.base.goFoiForward();
   }
-
   doGoBack() {
     this.base.goFoiBack();
+  }
+
+  get showAlert(): Boolean {
+    return this.answerReceived && !this.answerYes;
   }
 
 }
