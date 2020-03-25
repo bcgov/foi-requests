8e95871a6dd6c9ec0e525d47ab69042734f0c2e8
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2020-03-25 13:34:08 -0700

    Changes for config to read from api

--- src/app/services/keycloak.service.ts
+++ src/app/services/keycloak.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';
-import { default as config } from './keycloak-config.json';
 import { KeycloakLoginOptions } from 'keycloak-js';
+import {AppConfigService} from './app-config.service';
 
 declare var Keycloak: any;
 
@@ -12,10 +12,26 @@ declare var Keycloak: any;
 export class KeycloakService {
 
   private keycloakAuth: any;
+  private keycloakConfig: any;
+
+  constructor(private configService: AppConfigService) {
+
+  }
 
   init(): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.keycloakAuth = new Keycloak(config);
+      const config = this.configService.settings;
+      this.keycloakConfig = {
+        realm : config.realm,
+        url: config.url,
+        clientId: config.clientId,
+        credentials: {
+          secret: config.secret,
+          'ssl-required': config.sslRequired,
+          'public-client': config.publicClient
+        }
+      };
+      this.keycloakAuth = new Keycloak(this.keycloakConfig);
       const kcLogin = this.keycloakAuth.login;
       this.keycloakAuth.login = (options?: KeycloakLoginOptions) => {
         options.idpHint = 'bcsc';
@@ -46,7 +62,8 @@ export class KeycloakService {
 
   refreshToken() {
     if (!this.keycloakAuth) {
-      this.keycloakAuth = new Keycloak(config);
+
+      this.keycloakAuth = new Keycloak(this.keycloakConfig);
       this.keycloakAuth.init(
         {
           token: sessionStorage.getItem('KC_TOKEN'),
@@ -83,7 +100,7 @@ export class KeycloakService {
       };
   }
 
-  isAuthenticated(): boolean{
+  isAuthenticated(): boolean {
     const token = this.getDecodedToken();
     return token !== undefined && token.sub !== undefined;
   }
@@ -91,7 +108,7 @@ export class KeycloakService {
   logout() {
     if (this.isAuthenticated()) {
       if (!this.keycloakAuth) {
-        this.keycloakAuth = new Keycloak(config);
+        this.keycloakAuth = new Keycloak(this.keycloakConfig);
         this.keycloakAuth.init({token: sessionStorage.getItem('KC_TOKEN'), onLoad: 'check-sso'})
         .success(authenticated => {
           if (authenticated) {
