56f11ea78a9d341c76916ce91feec2b654bc193f
Abin Antony <abin.antony@aot-technologies.com>
2022-10-12 15:44:14 -0700

    #2861 Foster parent selection persistence for API

--- web/src/app/route-components/foster-parent/foster-parent.component.ts
+++ web/src/app/route-components/foster-parent/foster-parent.component.ts
@@ -6,6 +6,7 @@ import { DataService } from 'src/app/services/data.service';
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Router } from "@angular/router";
+import { componentNeedsResolution } from '@angular/core/src/metadata/resource_loading';
 
 @Component({
   templateUrl: './foster-parent.component.html',
@@ -22,28 +23,35 @@ export class FosterParent implements OnInit {
   foiRequest: FoiRequest;
   targetKey: string = "fosterparent";
   fulllistoptions: Observable<any>;
-  mainoptions:Array<any>;
-  checkstates:Array<string> = ['adoption','childprotectionchild','childprotectionparent','fosterparent','youthincarechild','youthincareparent'];
-  constructor(private fb: FormBuilder, private dataService: DataService,private route:Router) {}
+  mainoptions: Array<any>;
+  checkstates: Array<string> = ['adoption', 'childprotectionchild', 'childprotectionparent', 'fosterparent', 'youthincarechild', 'youthincareparent'];
+  constructor(private fb: FormBuilder, private dataService: DataService, private route: Router) { }
 
   ngOnInit() {
-  
-  
+
+
     // Load the current values & populate the FormGroup.
     this.foiRequest = this.dataService.getCurrentState(this.targetKey);
     this.foiForm.patchValue(this.foiRequest.requestData[this.targetKey]);
 
-    console.log(`ngOnInit-fosterparent-selectedtopics ${JSON.stringify(this.foiRequest.requestData.selectedtopics)}`)
 
-    let selectedoptions = this.foiRequest.requestData[this.targetKey].selectedoptions;
+
+    let selectedoptions = this.foiRequest.requestData.requestType.fosterparent;
 
     this.fulllistoptions = this.dataService.getFosterParent().pipe(
-      map(mainoptions=>{
-          mainoptions.forEach(mainoption =>{
-            mainoption.selected =  mainoption.selected || (selectedoptions ? !! selectedoptions.find(ms => ms.selected === mainoption.selected) : false);
-          })
+      map(_mainoptions => {
+        _mainoptions.forEach(_mainoption => {
+          _mainoption.selected = _mainoption.selected || (selectedoptions ? !!selectedoptions.find(smo=>smo.mainoption === _mainoption.mainoption) : false);
+          
+          let _suboptions = _mainoption.suboptions
+          let selectedmainoption= selectedoptions.find(smo=>smo.mainoption === _mainoption.mainoption)
+         
+          _suboptions.forEach(_suboption => {
+             _suboption.selected =  (selectedmainoption ? !!selectedmainoption.suboptions.find(sso => sso.option === _suboption.option && sso.selected === true):false)
+           })
+        })
 
-          return mainoptions;
+        return _mainoptions;
       }),
       map(mainoptions => {
         this.mainoptions = mainoptions;
@@ -52,14 +60,12 @@ export class FosterParent implements OnInit {
     );
 
   }
-  showsubsection(item:any)
-  {
-    item.selected=!item.selected   
+  showsubsection(item: any) {
+    item.selected = !item.selected
   }
 
-  selectedsuboption(item:any)
-  {
-    item.selected=!item.selected 
+  selectedsuboption(item: any) {
+    item.selected = !item.selected
 
   }
 
@@ -68,9 +74,10 @@ export class FosterParent implements OnInit {
 
     const formData = this.foiForm.value;
 
-    let selected = this.mainoptions.filter(m => m.selected);
-    this.foiRequest.requestData[this.targetKey].selectedoptions = selected; 
+    let selected = this.mainoptions.filter(m => m.selected === true);
     
+    this.foiRequest.requestData.requestType.fosterparent = selected;
+   
     // Update save data & proceed.
     this.dataService.setCurrentState(this.foiRequest, this.targetKey, this.foiForm);
     //this.base.goFoiForward();
@@ -78,57 +85,49 @@ export class FosterParent implements OnInit {
   }
 
   doGoBack() {
-   // this.base.goFoiBack();
-   this.rewindforSelectedPersonalTopics();
+    // this.base.goFoiBack();
+    this.rewindforSelectedPersonalTopics();
   }
 
-  forwardforSelectedPersonalTopics()
-  {
-    if(this.foiRequest.requestData.selectedtopics!=undefined && this.foiRequest.requestData.selectedtopics.length > 0)
-    {
-      
-      let current = this.foiRequest.requestData.selectedtopics.find(st=>st.value === this.targetKey)
+  forwardforSelectedPersonalTopics() {
+    if (this.foiRequest.requestData.selectedtopics != undefined && this.foiRequest.requestData.selectedtopics.length > 0) {
+
+      let current = this.foiRequest.requestData.selectedtopics.find(st => st.value === this.targetKey)
       let ci = this.foiRequest.requestData.selectedtopics.indexOf(current)
-      let next = this.foiRequest.requestData.selectedtopics[ci+1];
-      console.log(`next childprotectionparent : ${JSON.stringify(next)}`)
-      if(next!=undefined  && this.checkstates.includes(next.value))
-      {
+      let next = this.foiRequest.requestData.selectedtopics[ci + 1];
+
+      if (next != undefined && this.checkstates.includes(next.value)) {
         this.route.navigate([`/personal/${next.value}`])
       }
-      else{
+      else {
         this.base.goFoiForward();
       }
-        
+
     }
-    else
-    {
+    else {
       this.base.goFoiForward();
     }
   }
 
 
-  rewindforSelectedPersonalTopics()
-  {
-    if(this.foiRequest.requestData.selectedtopics!=undefined && this.foiRequest.requestData.selectedtopics.length > 0)
-    {
-      
-      let current = this.foiRequest.requestData.selectedtopics.find(st=>st.value === this.targetKey)
+  rewindforSelectedPersonalTopics() {
+    if (this.foiRequest.requestData.selectedtopics != undefined && this.foiRequest.requestData.selectedtopics.length > 0) {
+
+      let current = this.foiRequest.requestData.selectedtopics.find(st => st.value === this.targetKey)
       let ci = this.foiRequest.requestData.selectedtopics.indexOf(current)
-      let previous = this.foiRequest.requestData.selectedtopics[ci-1];
-      console.log(`next childprotectionparent : ${JSON.stringify(previous)}`)
-      if(previous!=undefined)
-      {
+      let previous = this.foiRequest.requestData.selectedtopics[ci - 1];
+
+      if (previous != undefined) {
         this.route.navigate([`/personal/${previous.value}`])
       }
-      else{
+      else {
         this.base.goFoiBack();
       }
-        
+
     }
-    else
-    {
+    else {
       this.base.goFoiBack();
     }
   }
- 
+
 }
