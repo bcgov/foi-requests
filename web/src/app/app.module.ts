52affdd370fba05fc281a8214fa48bd0a8ba8bcf
Jocelyn Cabildo <Jocelyn.Cabildo@gov.bc.ca>
2024-09-10 13:30:58 -0700

    Update Angular 18 and Nodejs 20 dependencies

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,34 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreHeaderComponent } from './core-header/core-header.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { TransomApiClientService } from './transom-api-client.service';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxWebstorageModule } from 'ngx-webstorage';
-import { ServicesModule } from './services/services.module';
-import { RouteComponentsModule } from './route-components/route-components.module';
-import { FooterComponent } from './footer/footer.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SigninComponent } from './route-components/signin/signin.component';
-import {AppConfigService} from './services/app-config.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreHeaderComponent } from "./core-header/core-header.component";
+import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+import { TransomApiClientService } from "./transom-api-client.service";
+import { HttpClientModule } from "@angular/common/http";
+import { provideNgxWebstorage } from "ngx-webstorage";
+import { ServicesModule } from "./services/services.module";
+import { RouteComponentsModule } from "./route-components/route-components.module";
+import { FooterComponent } from "./footer/footer.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SigninComponent } from "./route-components/signin/signin.component";
+import { AppConfigService } from "./services/app-config.service";
 
 export function init_app(appConfigService: AppConfigService) {
   return () => appConfigService.load();
 }
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoreHeaderComponent,
-    ProgressBarComponent,
-    FooterComponent,
-    SigninComponent
-  ],
+  declarations: [AppComponent, CoreHeaderComponent, ProgressBarComponent, FooterComponent, SigninComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -36,13 +29,14 @@ export function init_app(appConfigService: AppConfigService) {
     HttpClientModule,
     ServicesModule,
     RouteComponentsModule,
-    NgxWebstorageModule.forRoot(),
-    ReactiveFormsModule
+    // NgxWebstorageModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [
     TransomApiClientService,
     AppConfigService,
-    { provide: APP_INITIALIZER, useFactory: init_app, deps: [AppConfigService], multi: true }
+    { provide: APP_INITIALIZER, useFactory: init_app, deps: [AppConfigService], multi: true },
+    provideNgxWebstorage(),
 
     // {
     //   provide: APP_INITIALIZER,
@@ -51,6 +45,6 @@ export function init_app(appConfigService: AppConfigService) {
     //   multi: true
     // }
   ],
-  bootstrap: [AppComponent, FooterComponent]
+  bootstrap: [AppComponent, FooterComponent],
 })
-export class AppModule { }
+export class AppModule {}
