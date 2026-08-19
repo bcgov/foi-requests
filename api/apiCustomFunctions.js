7277987e155e2580ba9b145acac457693a53ca2d
Hundal <aman.hundal@gov.bc.ca>
2026-08-19 11:30:35 -0700

    revert of previous commits work

--- api/apiCustomFunctions.js
+++ api/apiCustomFunctions.js
@@ -435,12 +435,11 @@ const sendEmail = async (foiHtml, foiAttachments, server, inbox, subject, req) =
 
       const response = await new Promise((resolve, reject) => {
         transomMailer.sendFromNoReply(emailConfig, (err, response) => {
-          // if (err) reject(err);
-          // result.message = "Email \"" + subject + "\" Sent Successfully";
-          // result.EmailSuccess = true;
-          // req.log.info('EmailSent:', response);
-          // resolve(response);
-          reject(err)
+          if (err) reject(err);
+          result.message = "Email \"" + subject + "\" Sent Successfully";
+          result.EmailSuccess = true;
+          req.log.info('EmailSent:', response);
+          resolve(response);
         })
       });
 
