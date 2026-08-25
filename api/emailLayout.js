4e33e09fdd6a5c9b6db41da39f33c4d9352605dd
Hundal <aman.hundal@gov.bc.ca>
2026-08-25 10:23:07 -0700

    observation 1 and 5 fix

--- api/emailLayout.js
+++ api/emailLayout.js
@@ -471,7 +471,7 @@ function EmailLayout() {
     }
 
     // Simple footer
-    content += this.tableHeader(`Submitted ${new Date().toString()}`);
+    content += this.tableHeader(`Submitted ${(new Date).toLocaleDateString("en-US", {weekday: "long", timeZone: "America/Los_Angeles", year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit"})}`);
     content = this.table(content);
     // End of the Table
 
